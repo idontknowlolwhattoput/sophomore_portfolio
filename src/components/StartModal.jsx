9e99aa8c57da28692300da8421aa60fbe0d8cabf
josh <vergarajoshuamiguel@gmail.com>
2025-06-25 23:35:48 +0800

    wala lang

--- src/components/StartModal.jsx
+++ src/components/StartModal.jsx
@@ -2,7 +2,7 @@
 function StartModal() {
     return (
         <div className="bg-white w-30 h-50 z-99">
-       
+          <p>hellogi</p>
         </div>
     )
 }
--- src/pages/Win95home.jsx
+++ src/pages/Win95home.jsx
@@ -28,7 +28,7 @@ function App() {
   return (
     <>
       <div>
-      <div className="flex flex-col pl-5 pt-8 gap-3 w-screen h-screen">
+      <div className="flex flex-col pl-3 pt-6 gap-3 w-screen h-screen">
       {icons.map((ico, key) => (
         <div className="flex flex-col gap-1 items-center w-25 h-25 active:bg-gray-300 " key={key}>
           <img src={imageMap[ico.src]} className="w-15 h-15" />
