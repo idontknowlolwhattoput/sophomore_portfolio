94d1d76b47c15f6326f75fa5499ff56b6fef5449
idontknowlolwhattoput <132071917+idontknowlolwhattoput@users.noreply.github.com>
2025-06-29 00:11:14 -0700

    fixed the cursor

--- src/pages/Win95home.jsx
+++ src/pages/Win95home.jsx
@@ -18,7 +18,8 @@ const imageMap = {
 };
 
 function App() {
-
+  
+  const [isOpen, setOpen] = useState(true);
   const [isToggled, setToggle] = useState(false);
   const [currentTime, setCurrentTime] = useState(new Date());
 
@@ -64,16 +65,16 @@ return (
 <>
   {/* START MENU MODAL */}
   {isToggled && (
-  <div className="flex flex-row absolute h-100 w-75 bg-[#C0C0C0] bottom-13 z-40 shadow-[2px_2px_3px_0.5px_rgba(0,0,0,0.9)]">
-      <span className= "flex items-center justify-center bg-gray-600 w-12 ">
+  <div className="flex flex-row absolute h-100 w-75 bg-[#C0C0C0] bottom-13 z-40 shadow-[2px_2px_3px_0.5px_rgba(0,0,0,0.9)] border-3 border-white">
+      <span className= "flex items-center justify-center bg-gray-600 w-12">
         <div className="flex flex-col gap-26  mt-20">
-           <p className=" -rotate-90 text-gray-400 text-3xl font-semilight   whitespace-nowrap ">95</p>
-          <p className=" -rotate-90 text-gray-400 text-3xl font-extrabold whitespace-nowrap ">Windows</p>
+           <p className=" -rotate-90 text-gray-300 text-3xl font-semilight   whitespace-nowrap ">95</p>
+          <p className=" -rotate-90 text-gray-300 text-3xl font-extrabold whitespace-nowrap ">Windows</p>
         </div>
       </span>
-      <div className="pixel text-lg font-medium flex flex-col h-full w-full">
+      <div className="pixel text-lg font-medium flex flex-col h-full w-full cursor-pointer">
         {icons.map((ico, key) => (
-           <div className="flex items-center w-full h-13 hover:bg-[#0000ff] pl-2  " key={key}> 
+           <div className="flex items-center w-full h-13 pl-4 hover:bg-[#0000ff] hover:text-white" key={key}> 
                <img src={imageMap[ico.src]} className="w-8 h-8 mr-2" />
                <p>{ico.iconname}</p>
 
@@ -81,6 +82,10 @@ return (
         ))}
       </div>
   </div>
+  )}
+
+  {isOpen && (
+     <div className="bg-white h-10 w-20 absolute top-10 left-40"><p>d</p></div>
   )}
 
   {/* WINDOW AREA */}
@@ -98,9 +103,9 @@ return (
   {/* NAVBAR */}
   <div className="bottom-0">
     <div
-      className="flex justify-between items-center fixed bottom-0 w-full h-[8vh] pl-5 pr-5 bg-[#C0C0C0] border-t-4 border-white"
+      className="flex justify-between items-center fixed bottom-0 w-full h-[8vh] pl-5 pr-5 bg-[#C0C0C0] border-t-4 border-white">
+      <div className="flex flex-row gap-3 cursor-pointer"
       onClick={handleClick}>
-      <div className="flex flex-row gap-3">
         <motion.div whileTap={{ y: 2, scale: 0.96 }} transition={{
             type: "spring",
             stiffness: 300,
@@ -113,7 +118,7 @@ return (
       </div>
 
       <div
-        className="flex items-center font-regular justify-center gap-3 bg-[#C0C0C0] border-b-3 border-white border-r-3 h-10 w-30 shadow-[-2px_-2px_4px_2px_rgba(0,0,0,0.9)]">
+        className="flex items-center font-regular justify-center gap-3 bg-[#C0C0C0] border-b-3 border-white border-r-3 h-10 w-30 shadow-[-2px_-2px_4px_2px_rgba(0,0,0,0.9)] cursor-pointer">
         <h1 className="pixel tracking-widest text-black text-lg spacing">
           {currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
         </h1>
